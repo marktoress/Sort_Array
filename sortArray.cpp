c5e9c26a553aea246ef71b8b774e6dbabd74aeeb
viktor <victor.kutsiy@ya.ru>
2020-02-23 23:27:34 +0600

    Add Russian Language

--- sortArray.cpp
+++ sortArray.cpp
@@ -12,8 +12,9 @@ using namespace std;
 
 int main()
 {
-	
-	setlocale(LC_ALL,"rus");
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	srand(time(NULL));
 	const int m = 5;
 	const int n = 5;
